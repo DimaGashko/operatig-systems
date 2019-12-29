afe94dc2c74360694655db6dcbf1199912486a43
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-29 05:30:44 +0200

    base UI

--- 2.5-area/area.c
+++ 2.5-area/area.c
@@ -29,16 +29,16 @@ void run() {
       scanf("%s", cmd);
 
       if (strcasecmp(cmd, "square") == 0) {
-         square();
+         runSquare();
       }
-      if (strcasecmp(cmd, "rectangle") == 0) {
-         rectangle();
+      else if (strcasecmp(cmd, "rectangle") == 0) {
+         runRectangle();
       }
-      if (strcasecmp(cmd, "triangle") == 0) {
-         triangle();
+      else if (strcasecmp(cmd, "triangle") == 0) {
+         runTriangle();
       }
-      if (strcasecmp(cmd, "circle") == 0) {
-         circle();
+      else if (strcasecmp(cmd, "circle") == 0) {
+         runCircle();
       }
       else if (strcasecmp(cmd, "help") == 0) {
          help();
@@ -46,25 +46,35 @@ void run() {
       else if (strcasecmp(cmd, "exit") == 0) {
          break;
       }
+      else {
+         printf("Unknown command. Enter help to see list of commands\n");
+      }
    }
 
    printf("Have a good day!\n");
 }
 
 void runSquare() {
-
+   printf("Enter variables: a\n");
+   printf("a = \n");
 }
 
 void runRectangle() {
-   
+   printf("Enter variables: a, b\n");
+   printf("a = \n");
+   printf("b = \n");
 }
 
 void runTriangle() {
-   
+   printf("Enter variables: a, b, c\n");
+   printf("a = \n");
+   printf("b = \n");
+   printf("c = \n");
 }
 
 void runCircle() {
-   
+   printf("Enter variables: r\n");
+   printf("r = \n");
 }
 
 void square() {
@@ -82,7 +92,6 @@ void triangle() {
 void circle() {
    
 }
-
 
 void help() {
    printf("Find the area of some figures \n");
