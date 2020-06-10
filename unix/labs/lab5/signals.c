6d570f9cc737c39eddbda2223eb9df9154d8c9ce
Dmitry Gashko <dmitry.gashko@postindustria.com>
2020-06-10 05:41:52 +0300

    l5

---
+++ unix/labs/lab5/signals.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int count = 0;
+
+void signalsHandler() {
+   printf("SIGINT is received %d times\n", ++count);
+}
+
+int main() {
+   signal(SIGINT, signalsHandler);
+
+   while (1) {
+      sleep(10);
+   }
+
+   return 0;
+}
